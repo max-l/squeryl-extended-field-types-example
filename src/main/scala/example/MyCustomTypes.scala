c6806875ca543d0e83595c6a98b843eb6f662e15
Maxime Lévesque <maxime.levesque@gmail.com>
2011-12-13 12:11:19 -0500

    doc

--- src/main/scala/example/MyCustomTypes.scala
+++ src/main/scala/example/MyCustomTypes.scala
@@ -7,19 +7,55 @@ import java.sql.Timestamp
 import java.sql.ResultSet
 import org.squeryl.adapters.H2Adapter
 
+/**
+ * PrimitiveTypeMode being a trait, can be extended to   
+ * support additionnal field types than what the native JDBC. 
+ *
+ *  To support a non JDBC native type, one extends PrimitiveTypeMode
+ *  and adds 4 new implicits in it's scope.   Then all aspects 
+ *  of the Query DSL that are available for the native type 
+ *  becomes available for the new type 
+ *  
+ *  
+ */
 object MyCustomTypes extends PrimitiveTypeMode {
 
+  /**
+   * The 3 type arguments are :
+   * 1) Timestamp : the native JDBC type that will back our custom type (in this case JodaTime's DateTime)
+   * 2) The new type we want to support
+   * 3) One of the sealed traits defined here :  
+   *   https://github.com/max-l/Squeryl/blob/AST-lifting-overhaul/src/main/scala/org/squeryl/dsl/TypedExpression.scala#L28
+   * This will define the behavior of the new type, we want that in the DSL it behaves like 
+   * a timestamp, this will for example enable comparison operators (<, >, between, etc...)
+   * as well as min, max functions, if we chose TBoolean, then min max is not available,
+   * if we chose TBigDecimal (of TFloat), then avg, sum, etc become available.  
+   *  
+   */
+  
   implicit val jodaTimeTEF = new NonPrimitiveJdbcMapper[Timestamp, DateTime, TTimestamp](timestampTEF, this) {
+    
+    /**
+     * Here we implement functions fo convert to and from the native JDBC type
+     */
+    
     def convertFromJdbc(t: Timestamp) = new DateTime(t)
     def convertToJdbc(t: DateTime) = new Timestamp(t.getMillis())
   }
 
-  implicit def jodaTimeToTE(s: DateTime) = jodaTimeTEF.create(s)
-
+  /**
+   * We define this one here to allow working with Option of our new type, this allso 
+   * allows the 'nvl' function to work  
+   */
   implicit val optionJodaTimeTEF = new TypedExpressionFactory[Option[DateTime], TOptionTimestamp] with DeOptionizer[DateTime, TTimestamp, Option[DateTime], TOptionTimestamp] {
 
     val deOptionizer = jodaTimeTEF
   }
+  
+  /**
+   * the following are necessary for the AST lifting  
+   */
+  implicit def jodaTimeToTE(s: DateTime) = jodaTimeTEF.create(s)  
 
   implicit def optionJodaTimeToTE(s: Option[DateTime]) = optionJodaTimeTEF.create(s)
 
@@ -71,6 +107,7 @@ object JodaTimeTests {
       from(timestampTester)(tt =>
         where(tt.time < b5)
         select (&(tt.time)))
+        
     assert(x1.size == 1)
 
     println(x1.mkString("\n"))
@@ -79,6 +116,7 @@ object JodaTimeTests {
       from(timestampTester)(tt =>
         where(tt.time >= b5)
           select (tt))
+          
     assert(x2.size == 2)
 
     println(x2.map(_.time).mkString("\n"))
